b7f23e3da5eacb205983fa53faf46ef70b3b1665
rixcian <rostislav.kremecek@protonmail.com>
2021-12-02 13:48:57 +0100

    refactor: update docker setup

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ echo "[DEPLOY.SH] DONE"
 
 echo "[DEPLOY.SH] Building new image"
 export IMAGE_TAG=$(date +"%Y_%m_%d-%H_%M")-$(git rev-parse --short HEAD)
-docker build -t node_typescript_api_starter:$IMAGE_TAG .
+docker build -t bankless_mailchimp_api:$IMAGE_TAG .
 echo "[DEPLOY.SH] DONE"
 
 echo "[DEPLOY.SH] Deleting old container & running new one"
 docker rm -f api_starter
-docker run --detach --restart=always --env-file .env -p 8000:8000 --name api_starter node_typescript_api_starter:$IMAGE_TAG
+docker run --detach --restart=always --env-file .env -p 8001:8001 --name bankless_mailchimp_api bankless_mailchimp_api:$IMAGE_TAG
 echo "[DEPLOY.SH] DONE"
 
 echo " "
